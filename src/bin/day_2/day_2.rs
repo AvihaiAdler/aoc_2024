a98f142f14889b2d05d47e43f759d81f7d45d9dc
adler <avihai.adler@gmail.com>
2024-12-23 02:14:09 +0200

    broke down 'monolith' functions for modularity

--- src/bin/day_2/day_2.rs
+++ src/bin/day_2/day_2.rs
@@ -7,7 +7,7 @@ fn main() -> io::Result<()> {
     let reader = get_reader("resources/day_2.txt")?;
 
     let data = get_data(reader);
-    println!("safe reports: {}", check_diffs_safety(&data));
+    println!("safe reports: {}", check_safety(&data));
     println!("relaxed reports: {}", check_safety_relaxed(&data));
     Ok(())
 }
@@ -24,25 +24,10 @@ fn get_data(reader: BufReader<File>) -> Vec<Vec<i8>> {
         .collect()
 }
 
-fn get_diffs(data: &[Vec<i8>]) -> Vec<Vec<i8>> {
-    data.iter()
-        .map(|v| {
-            v.windows(2)
-                .map(|window| window[0] - window[1])
-                .collect::<Vec<_>>()
-        })
+fn get_diff(data: &[i8]) -> Vec<i8> {
+    data.windows(2)
+        .map(|window| window[0] - window[1])
         .collect::<Vec<_>>()
-}
-
-fn check_diffs_safety(data: &[Vec<i8>]) -> usize {
-    let diffs = get_diffs(data);
-
-    // filter out all the diffs where 2 adjecent diffs have different signedness
-    diffs
-        .iter()
-        .filter(|&diff| diff.windows(2).all(|window| window[0] * window[1] > 0))
-        .filter(|&diff| diff.iter().all(|&e| e.abs() < 4))
-        .count()
 }
 
 fn check_diff_safety(diffs: &[i8]) -> bool {
@@ -52,10 +37,15 @@ fn check_diff_safety(diffs: &[i8]) -> bool {
     signedness & limit_exceeded
 }
 
-fn get_diff(data: &[i8]) -> Vec<i8> {
-    data.windows(2)
-        .map(|window| window[0] - window[1])
-        .collect::<Vec<_>>()
+/**
+ * checks for 'safe reports'. a 'safe report' is defined to be a report where all the differences between 2 adjacent elements have the same singedness,
+ * and no difference is larger than 3    
+ */
+fn check_safety(data: &[Vec<i8>]) -> usize {
+    data.iter()
+        .map(|vec| get_diff(vec))
+        .map(|diff| check_diff_safety(&diff))
+        .fold(0, |acc, res| if res { acc + 1 } else { acc })
 }
 
 /**
@@ -76,49 +66,4 @@ fn check_safety_relaxed(data: &[Vec<i8>]) -> usize {
         }
     }
     res
-}
-
-#[allow(unused)]
-fn check_safety_lossly(data: &[Vec<i8>]) -> usize {
-    let diffs = get_diffs(data);
-
-    // TODO
-    let fluctuating_diffs = diffs
-        .iter()
-        .filter(|&diff| diff.windows(2).any(|window| window[0] * window[1] <= 0))
-        .filter(|&diff| {
-            // filter all fluctuating diffs with more than 1 fluctuation
-            diff.windows(2)
-                .map(|window| window[0] * window[1])
-                .filter(|&e| e < 0)
-                .count()
-                > 1
-        })
-        .cloned()
-        .collect::<Vec<_>>();
-
-    let sum_of_fluctuating_diffs = fluctuating_diffs
-        .iter()
-        .filter(|&diff| diff.iter().all(|&e| e.abs() <= 3))
-        .map(|diff| {
-            diff.windows(2)
-                .map(|window| window[0] + window[1])
-                .collect::<Vec<_>>()
-        })
-        .count();
-
-    // get all sum of all unsafe diffs. filter out all 'fluctuating' diffs
-    let sum_of_diffs = diffs
-        .iter()
-        .filter(|&diff| diff.windows(2).all(|window| window[0] * window[1] > 0))
-        .filter(|&diff| diff.iter().any(|&e| e.abs() > 3))
-        .map(|diff| {
-            diff.windows(2)
-                .map(|window| window[0] + window[1])
-                .collect::<Vec<_>>()
-        })
-        .filter(|diff| diff.iter().filter(|&e| e.abs() > 3).count() > 1) // filter out all of the sums of diffs who contain more than one 'usafe value'. i.e. [1, 2, 4, 4] will be filtered out
-        .collect::<Vec<_>>();
-
-    sum_of_diffs.len() + sum_of_fluctuating_diffs
 }
